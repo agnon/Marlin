93e653bdcd1af68a432a7c61d632f738b89308f4
Marcus Nordenberg <marcus.nordenberg@gmail.com>
2024-05-02 21:54:14 +0200

    Update bed size to reflect reality :)
    Maybe X size can be increased after moving the PSU...

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1825,8 +1825,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 233
+#define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
