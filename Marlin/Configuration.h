774f19c21b3a668ff9d1b167ac2fad400ce04690
Marcus Nordenberg <marcus.nordenberg@gmail.com>
2024-06-17 22:26:55 +0200

    Decrease y bed size a notch
    The new cooling case might hit the cable connector for the heated bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1826,7 +1826,7 @@
 
 // The size of the printable area
 #define X_BED_SIZE 233
-#define Y_BED_SIZE 235
+#define Y_BED_SIZE 233
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
